2f476e304e08b5921705aba93970052108a97981
dengnh <dengninghai@shinemo.com>
2024-01-17 14:38:42 +0800

    test

--- packages/tools/rollup.config.js
+++ packages/tools/rollup.config.js
@@ -1,41 +1,41 @@
-import { defineConfig } from "rollup";
-import typescript from "rollup-plugin-typescript2"; // 处理typescript
-import babel from "@rollup/plugin-babel";
-import dts from "rollup-plugin-dts";
+import { defineConfig } from 'rollup';
+import typescript from 'rollup-plugin-typescript2'; // 处理typescript
+import babel from '@rollup/plugin-babel';
+import dts from 'rollup-plugin-dts';
 
 export default defineConfig([
   {
-    input: "./index.ts",
+    input: './index.ts',
     plugins: [
       typescript(), // typescript 转义
       babel({
         babelrc: false,
-        presets: [["@babel/preset-env", { modules: false, loose: true }]],
-        plugins: [["@babel/plugin-proposal-class-properties", { loose: true }]],
-        exclude: "node_modules/**",
-      }),
+        presets: [['@babel/preset-env', { modules: false, loose: true }]],
+        plugins: [['@babel/plugin-proposal-class-properties', { loose: true }]],
+        exclude: 'node_modules/**'
+      })
     ],
     output: [
-      { file: "dist/index.js", format: "cjs" },
-      { file: "dist/index.esm.js", format: "es" },
+      { file: 'dist/index.js', format: 'cjs' },
+      { file: 'dist/index.esm.js', format: 'es' },
       {
-        format: "module",
-        file: "dist/index.esm.js",
+        format: 'module',
+        file: 'dist/index.esm.js'
       },
       {
-        format: "umd",
-        file: "dist/index.min.js",
-        name: "PackageName",
-      },
-    ],
+        format: 'umd',
+        file: 'dist/index.min.js',
+        name: 'PackageName'
+      }
+    ]
   },
   /* 单独生成声明文件 */
   {
-    input: "./index.ts",
+    input: './index.ts',
     plugins: [dts()],
     output: {
-      format: "esm",
-      file: "dist/index.d.ts",
-    },
-  },
+      format: 'esm',
+      file: 'dist/index.d.ts'
+    }
+  }
 ]);
